99e0ba82c430a90993d5f34b056f6d5571aee22d
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2016-04-28 10:39:29 +0530

    Update hamming_code.c

--- hamming_code.c
+++ hamming_code.c
@@ -1,50 +1,94 @@
-#include<stdlib.h>
 #include<stdio.h>
-char data[5];
-int encoded[8], edata[7], syndrome[3];
-int hmatrix[3][7]= { 1,0,0,0,1,1,1,
-0,1,0,1,0,1,1,
-0,0,1,1,1,0,1};
-char gmatrix[4][8]={ "0111000", "1010100", "1100010", "1110001"};
-int main()
-{
-int i,j;
-system("clear");
-printf("Hamming Code --- Encoding\n");
-printf("Enter 4 bit data : ");
-scanf("%s",data);
-printf("Generator Matrix\n");
-for(i=0;i<4;i++)
-printf("\t %s \n",gmatrix[i]);
-printf("Encoded Data : ");
-for(i=0;i<7;i++)
-{
-for(j=0;j<4;j++)
-encoded[i]+=((data[j]- '0')*(gmatrix[j][i]- '0'));
-encoded[i]=encoded[i]%2;
-printf("%d",encoded[i]);
-}
-printf("\nHamming code --- Decoding\n");
-printf("Enter Encoded bits as received : ");
-for(i=0;i<7;i++)
-scanf("%d",&edata[i]);
-for(i=0;i<3;i++)
-{
-for(j=0;j<7;j++)
-syndrome[i]=syndrome[i]+(edata[j]*hmatrix[i][j]);
-syndrome[i]=syndrome[i]%2;
-}
-for(j=0;j<7;j++)
-if ((syndrome[0]==hmatrix[0][j])&&(syndrome[1]==hmatrix[1][j])&&
-(syndrome[2]==hmatrix[2][j]))
-break;
-if(j==7)
-printf("Data is error free!!\n");
-else {
-printf("Error received at bit number %d of the data\n",j+1);
-edata[j]=!edata[j];
-printf("The Correct data Should be : ");
-for(i=0;i<7;i++) printf(" %d ",edata[i]);
-}
-}
+#include<unistd.h>
 
+int generator_matrix[4][7] = {
+	0, 1, 1, 1, 0, 0, 0,
+	1, 0, 1, 0, 1, 0, 0,
+	1, 1, 0, 0, 0, 1, 0,
+	1, 1, 1, 0, 0, 0, 1
+};
+
+int hamming_matrix[3][7] = {
+	1, 0, 0, 0, 1, 1, 1,  
+	0, 1, 0, 1, 0, 1, 1,
+ 	0, 0, 1, 1, 1, 0, 1
+};
+
+int main(int argc, char const *argv[])
+{
+	int data[4], received[7];
+	int encoded[7] = {0};
+	int i = 0, j = 0;
+	int syndrome[3] = {0};
+
+	for (i = 0; i < 4; ++i)
+	{
+		scanf("%d", &data[i]);
+	}
+
+	for (i = 0; i < 4; ++i)
+	{
+		for (j = 0; j < 7; ++j)
+		{
+			encoded[j] += data[i] * generator_matrix[i][j];
+		}
+	}
+
+	for (i = 0; i < 7; ++i)
+	{
+		encoded[i] = encoded[i] % 2;
+	}
+
+	printf("%s\n", "encoded data");
+	for (i = 0; i < 7; ++i)
+	{
+		printf("%d\n", encoded[i]);
+	}
+
+	
+	printf("%s\n", "Enter received data");
+	for (i = 0; i < 7; i++)
+	{
+		scanf(" %d", &received[i]);
+	}
+
+	for (i = 0; i < 3; ++i)
+	{
+		for (j = 0; j < 7; ++j)
+		{
+			syndrome[i] +=	hamming_matrix[i][j] * received[j];
+		}
+	}
+
+	for (i = 0; i < 3; ++i)
+	{
+		syndrome[i] = syndrome[i] % 2;
+	}
+
+	for (i = 0; i < 7; )
+	{
+		if (syndrome[0] == hamming_matrix[0][i] &&
+			syndrome[1] == hamming_matrix[1][i] &&
+			syndrome[2] == hamming_matrix[2][i])
+		{
+			break;
+		}
+	}
+
+	if (i == 7)
+	{
+		printf("%s\n", "error free data");
+		return 0;
+	}
+
+	printf("%s %d\n", "Error bit is", i + 1);
+	received[i] = !received[i];
+
+	printf("%s\n", "Error free data is");
+	for (i = 0; i < 7; ++i)
+	{
+		printf("%d\n", received[i]);
+	}
+
+	return 0;
+}
